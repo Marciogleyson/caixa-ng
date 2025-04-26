514817317f6392fd70ddeae5731dfb476d30d10d
Marcio Ramos <marcio.gleyson2026@gmail.com>
2025-04-25 21:49:54 -0300

    permitir cadastrar receita

---
+++ src/app/components/receitas/cadastro-receita/cadastro-receita.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-receita',
+  imports: [FormsModule],   // quando usar ngModulo tem que usar o import
+  templateUrl: './cadastro-receita.component.html',
+  styleUrl: './cadastro-receita.component.css'
+})
+export class CadastroReceitaComponent {
+  // Toda vez que utilizar [{ngModel}] é obrigatório importar FormsModule
+  // ngModel é a forma que fazemos a ligação de algum campo com uma variável
+
+  nome: string = "";
+  valor: number = 0;
+  receita: Array <string> = []
+
+  salvarReceita(){
+    debugger;
+    // como adicionar um elemento em uma lista de string em ts
+    this.receitas.push(this.nome);
+    alert(this.nome);
+  }
+}
